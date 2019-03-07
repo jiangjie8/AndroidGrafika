b1d02faf09fcb668a45bbf54c09d1897031227db
jiangjie <jie.jiang@v.photos>
2019-03-07 11:37:52 +0800

    add

--- project/android/build.sh
+++ project/android/build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+
 ./gradlew clean assembleRelease
 rm -rf  /d/JIE/workspace/VCloudBox/app/libs/libmedia-release.aar
 mv libmedia/build/outputs/aar/libmedia-release.aar   /d/JIE/workspace/VCloudBox/app/libs/libmedia-release.aar
+
+git status
